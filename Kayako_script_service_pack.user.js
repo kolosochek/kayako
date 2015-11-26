ecc95b246c5d92713fbf08ecc74cde0d3a73868c
kolosochek <kolosochek@gmail.com>
2015-11-26 13:58:42 +0500

    Changed a bit observer feature, added target='_blank' to all links in tickets

--- Kayako_script_service_pack.user.js
+++ Kayako_script_service_pack.user.js
@@ -70,76 +70,141 @@ console.log(textarea);
 
 
 var settings = new Object({
-	'request_timeout': 4000,
+	'request_timeout': 5000,
 	'refresh_timeout': 15000, // send request every 15 seconds
 	'alert_file_url': "https://raw.githubusercontent.com/kolosochek/kayako/master/alert.mp3",
 	'alert_file_url_ogg': '',
+	'tickets_on_the_page_selector': "#ticketlist td.contenttableborder tr",
+	// backup trmassaction block because we got wrong encoding in response
+	mass_action_backup: document.getElementById('trmassaction'),
 	//'predefined_replies_url': "https://support.mchost.ru/support/staff/index.php?_m=tickets&_a=managepredreplies",
 });
 
-function set_observer(){
-	// debug
-	console.log('the observer is set');
-	// get ticket list on current page
-	var tickets_on_the_page = document.querySelectorAll("#ticketlist td.contenttableborder tr.row2");//"tr.rownotes");
-	// send request
-	var observer = setInterval(function(){
-		console.log('Send request:');
-		GM_xmlhttpRequest({
-			method:     "GET",
-			url:        window.location.href,
-			data:       "",
-			headers:    {
-				"Content-Type": "application/x-www-form-urlencoded"
-			},
-			onload: function (response) {
+var observer = new Object({
+	tickets_on_the_page: document.querySelectorAll(settings.tickets_on_the_page_selector),
+	timer: 0,
+	get_page_tickets: function(){
+		// get ticket list on current page
+		this.tickets_on_the_page = document.querySelectorAll(settings.tickets_on_the_page_selector);//"tr.row2");//"tr.rownotes");
+		/*var backup;
+		for(var i=0;i<this.tickets_on_the_page.length;i++){
+			if((i>0) && (i<this.tickets_on_the_page.length - 1)){
+				backup = this.tickets_on_the_page
+			}
+		}
+		*/
+	},
+	refresh_tickets_on_the_page: function(){
+		// don't forget to refresh tickets_on_the_page counter
+		this.tickets_on_the_page = document.querySelectorAll(settings.tickets_on_the_page_selector);
+		var tickets = this.tickets_on_the_page;
+		// set target="_blank" to all a elements in the ticket to open new tickets in separate tab and don't break the observer
+		for(i=0;i<tickets.length;i++){
+			if ((tickets[i].id != '') && (tickets[i].id != "trmassaction")) { 
+				var links = tickets[i].querySelectorAll("a"); 
+				for (j=0;j<links.length;j++)
+					{ links[j].target="_blank"}  
+			}
+		}
+		/* 
+		for(var i=0;i<this.tickets_on_the_page.length;i++){
+			//for(i=0;i<tickets.length;i++){ if ((tickets[i].id != '') && (tickets[i].id != "trmassaction")) { console.log(tickets[i].querySelector("a")) }}
+			// for(i=0;i<tickets.length;i++){ if ((tickets[i].id != '') && (tickets[i].id != "trmassaction")) { console.log(tickets[i]) }}
+			if ((this.tickets_on_the_page[i].id != '') && (this.tickets_on_the_page[i].id != "trmassaction")) {
+				var ticket_links = this.tickets_on_the_page[i].querySelectorAll('a');
 				// debug
-				//console.log(response);//.responseText);
-				
-				var chunk_form = '<form name="ticketlist" id="ticketlist" action="index.php" method="POST">'
-				var chunk_form_end = '</form>';
-				var form_html = response.responseText.substring(response.responseText.search(chunk_form), response.responseText.search(chunk_form_end));
-				form_html+= chunk_form_end;
-				// debug
-				//console.log(form_html);
-				var dom = document.createElement('div');
-				dom.innerHTML = form_html;
-				// store new data in dummy html element
-				var tickets_in_responce = dom.querySelectorAll("#ticketlist td.contenttableborder tr.row2");//"tr.rownotes");
-				// debug
-				console.log('already on the page: '+ tickets_on_the_page.length);
-				console.log('tickets in response: '+ tickets_in_responce.length);
-				if (tickets_on_the_page.length < tickets_in_responce.length){
-					// debug
-					console.log('Got new ticket!');
-					// play sound
-					play_sound();
-					// and then just apply new tickets to the page
-					document.getElementById('ticketlist').innerHTML = dom.innerHTML;
-					// don't forget to refresh tickets_on_the_page counter
-					tickets_on_the_page = document.querySelectorAll("#ticketlist td.contenttableborder tr.row2");
-				} else {
-					console.log("No new tickets");
+				console.log(ticket_links);
+				// if we have some links tickets on the page
+				if (ticket_links.length){
+					// let's iterate in and change target attribute
+					for(var i=0;i<this.ticket_links.length;i++){
+						// debug
+						console.log(ticket_links[i]);
+						ticket_links[i].target = '_blank';
+					}
 				}
-			},
-			ontimeout: function(){
-				// debug
-			    console.log('Timeout');
-			},
-			// request timeout
-			timeout: settings.request_timeout,
-		});
-	}, settings.refresh_timeout);
-}
+			}
+			
+		}
+		*/
+	},
+	set_observer: function (){
+		// debug
+		console.log('the observer sucsessfully set');
+		// get page tickets
+		this.tickets_on_the_page = document.querySelectorAll(settings.tickets_on_the_page_selector);//this.get_page_tickets();		
+		// send request
+		this.timer = setInterval(function(){
+			// debug
+			console.log('Send request: '+ window.location.href);
+			GM_xmlhttpRequest({
+				method:     "GET",
+				url:        window.location.href,
+				data:       "",
+				headers:    {
+					"Content-Type": "application/x-www-form-urlencoded"
+				},
+				onload: function (response) {
+					// debug
+					//console.log(response);//.responseText);					
+					var chunk_form = '<form name="ticketlist" id="ticketlist" action="index.php" method="POST">'
+					var chunk_form_end = '</form>';
+					var form_html = response.responseText.substring(response.responseText.search(chunk_form), response.responseText.search(chunk_form_end));
+					form_html+= chunk_form_end;
+					// debug
+					//console.log(form_html);
+					var dom = document.createElement('div');
+					dom.innerHTML = form_html;
+					// store new data in dummy html element
+					var tickets_in_response = dom.querySelectorAll(settings.tickets_on_the_page_selector);//"tr.rownotes");
+					// debug
+					console.log('tickets on the page: ' + (observer.tickets_on_the_page.length - 2));
+					console.log('tickets in response: ' + (tickets_in_response.length - 2));
+					// check results
+					if (observer.tickets_on_the_page.length < tickets_in_response.length){
+						// debug
+						console.log('Got new ticket!');
+						//
+						// play sound
+						play_sound();
+						// and then just apply new tickets to the page
+						document.getElementById('ticketlist').innerHTML = dom.innerHTML;
+						// restore trmassaction block from backup
+						settings.mass_action_backup.innerHTML = backup.innerHTML;
+						// refresh current tickets counter
+						observer.refresh_tickets_on_the_page();
+					} else if(observer.tickets_on_the_page.length > tickets_in_response.length) {
+						// insert loaded tickets
+						document.getElementById('ticketlist').innerHTML = dom.innerHTML;
+						// restore trmassaction block from backup
+						settings.mass_action_backup.innerHTML = backup.innerHTML;
+						// refresh current tickets counter
+						observer.refresh_tickets_on_the_page();
+					} else {
+						// debug
+						console.log("No new tickets");
+					}
+				},
+				ontimeout: function(){
+					// debug
+				    console.log('Timeout');
+				},
+				// request timeout
+				timeout: settings.request_timeout,
+			});
+		}, settings.refresh_timeout);
+	},
+	remove_observer: function(){
+		// debug
+		console.log('the observer sucsessfully removed');
+		clearInterval(this.timer);
+	},
+});
 
 function play_sound(){   
     document.getElementById("prcachemenu_r").innerHTML='<audio autoplay="autoplay"><source src="' + settings.alert_file_url + '" type="audio/mpeg" /><source src="' + settings.alert_file_url_ogg + '" type="audio/ogg" /><embed hidden="true" autostart="true" loop="false" src="' + settings.alert_file_url +'" /></audio>';
 }
-
-function remove_observer(){
-
-}
-
+// toolbox feature
 var toolbox = document.createElement('div');
 toolbox.id = 'toolbox';
 toolbox.style.position = 'absolute';
@@ -153,18 +218,15 @@ var button_remove_observer = document.getElementById('remove_observer');
 // bind event listener
 // set
 button_set_observer.addEventListener('click', function(){
-	set_observer();
+	button_set_observer.disabled = 'true';
+	button_remove_observer.disabled = '';
+	observer.set_observer();
 });
 // remove
 button_remove_observer.addEventListener('click', function(){
-	// debug
-	console.log('gotcha');
-	remove_observer();
+	button_set_observer.disabled = '';
+	button_remove_observer.disabled = 'true';
+	observer.remove_observer();
 });
 
-
-
-
-// debug
-//play_sound();
 //end 

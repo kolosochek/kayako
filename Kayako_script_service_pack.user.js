1e7f067837846ed114e56aa9a5d5e41b4f8e6cdf
kolosochek <kolosochek@gmail.com>
2015-11-26 11:43:35 +0500

    backup for working ticket observer feature

--- Kayako_script_service_pack.user.js
+++ Kayako_script_service_pack.user.js
@@ -16,11 +16,6 @@ document.body.appendChild(script);
 */
 //debug
 //console.log("gotcha");
-
-var settings = new Object({
-	'request_timeout': 4000,
-	'predefined_replies_url': "https://support.mchost.ru/support/staff/index.php?_m=tickets&_a=managepredreplies",
-});
 /*
 GM_xmlhttpRequest({
 	method:     "GET",
@@ -71,28 +66,105 @@ textarea.addEventListener('input', function(){
 // debug
 console.log(textarea);
 */
-// get ticket list on current page
-var tickets_on_the_page = document.querySelectorAll("tr.rownotes");
-// and then store it
 
-GM_xmlhttpRequest({
-	method:     "GET",
-	url:        window.location.href,
-	data:       "",
-	headers:    {
-		"Content-Type": "application/x-www-form-urlencoded"
-	},
-	onload: function (response) {
-		// debug
-		//console.log(response);//.responseText);
-		var tickets_in_responce = response.responseText.match(/class="rownotes"/);
-		console.log(tickets_in_responce);
-	},
-	ontimeout: function(){
-		// debug
-	    console.log('Timeout');
-	},
-	// request timeout
-	timeout: settings.request_timeout,
+
+
+var settings = new Object({
+	'request_timeout': 4000,
+	'refresh_timeout': 15000, // send request every 15 seconds
+	'alert_file_url': "https://raw.githubusercontent.com/kolosochek/kayako/master/alert.mp3",
+	'alert_file_url_ogg': '',
+	//'predefined_replies_url': "https://support.mchost.ru/support/staff/index.php?_m=tickets&_a=managepredreplies",
 });
+
+function set_observer(){
+	// debug
+	console.log('the observer is set');
+	// get ticket list on current page
+	var tickets_on_the_page = document.querySelectorAll("#ticketlist td.contenttableborder tr.row2");//"tr.rownotes");
+	// send request
+	var observer = setInterval(function(){
+		console.log('Send request:');
+		GM_xmlhttpRequest({
+			method:     "GET",
+			url:        window.location.href,
+			data:       "",
+			headers:    {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			onload: function (response) {
+				// debug
+				//console.log(response);//.responseText);
+				
+				var chunk_form = '<form name="ticketlist" id="ticketlist" action="index.php" method="POST">'
+				var chunk_form_end = '</form>';
+				var form_html = response.responseText.substring(response.responseText.search(chunk_form), response.responseText.search(chunk_form_end));
+				form_html+= chunk_form_end;
+				// debug
+				//console.log(form_html);
+				var dom = document.createElement('div');
+				dom.innerHTML = form_html;
+				// store new data in dummy html element
+				var tickets_in_responce = dom.querySelectorAll("#ticketlist td.contenttableborder tr.row2");//"tr.rownotes");
+				// debug
+				console.log('already on the page: '+ tickets_on_the_page.length);
+				console.log('tickets in response: '+ tickets_in_responce.length);
+				if (tickets_on_the_page.length < tickets_in_responce.length){
+					// debug
+					console.log('Got new ticket!');
+					// play sound
+					play_sound();
+					// and then just apply new tickets to the page
+					document.getElementById('ticketlist').innerHTML = dom.innerHTML;
+					// don't forget to refresh tickets_on_the_page counter
+					tickets_on_the_page = document.querySelectorAll("#ticketlist td.contenttableborder tr.row2");
+				} else {
+					console.log("No new tickets");
+				}
+			},
+			ontimeout: function(){
+				// debug
+			    console.log('Timeout');
+			},
+			// request timeout
+			timeout: settings.request_timeout,
+		});
+	}, settings.refresh_timeout);
+}
+
+function play_sound(){   
+    document.getElementById("prcachemenu_r").innerHTML='<audio autoplay="autoplay"><source src="' + settings.alert_file_url + '" type="audio/mpeg" /><source src="' + settings.alert_file_url_ogg + '" type="audio/ogg" /><embed hidden="true" autostart="true" loop="false" src="' + settings.alert_file_url +'" /></audio>';
+}
+
+function remove_observer(){
+
+}
+
+var toolbox = document.createElement('div');
+toolbox.id = 'toolbox';
+toolbox.style.position = 'absolute';
+var button_set_observer = '<button id="set_observer">Set observer</button>';
+var button_remove_observer = '<button id="remove_observer">Remove observer</button>';
+toolbox.innerHTML+=button_set_observer+button_remove_observer;
+document.body.insertBefore(toolbox, document.body.firstChild);
+// get created elements from dom
+var button_set_observer = document.getElementById('set_observer');
+var button_remove_observer = document.getElementById('remove_observer');
+// bind event listener
+// set
+button_set_observer.addEventListener('click', function(){
+	set_observer();
+});
+// remove
+button_remove_observer.addEventListener('click', function(){
+	// debug
+	console.log('gotcha');
+	remove_observer();
+});
+
+
+
+
+// debug
+//play_sound();
 //end 
